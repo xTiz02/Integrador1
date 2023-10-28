c7bd5bef42724e511145896b15e2fd4fd1b4423b
prdRep <u21231696@utp.edu.pe>
2023-10-28 14:59:28 -0500

    Primer Commit de la Libreria

---
+++ LibreriaSystem/src/main/java/com/example/libreriasystem/LibreriaSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.libreriasystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibreriaSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibreriaSystemApplication.class, args);
+    }
+
+}
---
+++ LibreriaSystem/src/test/java/com/example/libreriasystem/LibreriaSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.libreriasystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibreriaSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
